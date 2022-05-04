c7eed310fc6dc4446c613bea01da68142854d8d5
13403080864 <85783799+13403080864@users.noreply.github.com>
2022-05-04 11:44:33 +0800

    第3次提交5.4

---
+++ src/main/java/com/AL.java
@@ -0,0 +1,12 @@
+package com;
+
+/**
+ * @author wq
+ * @since 2022-05-04 上午11:43
+ */
+
+public class AL {
+    public static void main(String[] args) {
+        System.out.println("fha ");
+    }
+}
